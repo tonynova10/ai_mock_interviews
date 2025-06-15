d13051af506390b8ee5ede26e726ecd0512d8949
Tony Degollado <antoniodegollado10@gmail.com>
2025-06-14 21:08:53 -0600

    Added name to navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -20,7 +20,10 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
             <h2 className="text-primary-100">PrepWise</h2>
           </Link>
           <div className="ml-auto">
-            <SignOutButton />
+            <span className="mx-2">Hi, {user.name}</span>
+            <span>
+              <SignOutButton />
+            </span>
           </div>
         </div>
       </nav>
